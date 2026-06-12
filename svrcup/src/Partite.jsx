41cc46dd1505920211b6bcae3f20a1f9d0dc6e86
trinchierilucauni <trinchieri.2150132@studenti.uniroma1.it>
2026-06-12 15:58:38 +0200

    Fix Partite.jsx implementazione onClick per infoPartita e infosquadra

--- svrcup/src/Partite.jsx
+++ svrcup/src/Partite.jsx
@@ -1,10 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import logo from "./assets/logo.png";
 import React, {useEffect, useState} from "react";
 
 function Partite(){
     const [partite, setPartite] = useState([]);
-
+    const navigate= useNavigate();
     const chePartite = async () => {
         // Sostituito l'IP fisso con la variabile d'ambiente di Vite usando i backtick
         const response = await fetch(`${import.meta.env.VITE_API_URL}/api/partite`, {
@@ -24,6 +25,15 @@ function Partite(){
         const d = new Date(data);
         return d.toLocaleTimeString("it-IT", {hour: "2-digit", minute: "2-digit"});
     }
+    
+    const gestisciClickSuSquadra=(e, infoSquadra)=>{
+        e.stopPropagation();
+        navigate("/infosquadra", {state: infoSquadra, replace: true});
+    }
+
+    const gestisciClickSuPartita=(partitaInfo)=>{
+        navigate("/infopartita",{state: partitaInfo})
+    }
 
     useEffect(() => { chePartite(); }, []);
 
@@ -41,18 +51,18 @@ function Partite(){
                     {partite.map((key, index) => (
                         <React.Fragment key={key.id_partita}>
                             <div className="giorno-text">{cheGiorno(key.giorno)}</div>
-                            <div className="single-match-container">
+                            <div className="single-match-container" onClick={()=>gestisciClickSuPartita({id_partita: key.id_partita})}>
                                 <div className="left-part">
                                     <div className="squadre-name-container">
                                         <div className="squadra1-container">
-                                            <div className="squadra-info">
+                                            <div className="squadra-info"  onClick={(e) => { gestisciClickSuSquadra(e, {nome: key.squadra1, icona: key.icon1}) }}>
                                                 <div className="squadra1-partite-icon" style={{backgroundImage:`url(${key.icon1})`}}></div>
                                                 <div className="squadra1-name">{key.squadra1}</div>
                                             </div>
                                             <div className="squadra1-result">{key.risultato1 ?? "–"}</div>
                                         </div>
                                         <div className="squadra1-container">
-                                            <div className="squadra-info">
+                                            <div className="squadra-info"  onClick={(e) => {gestisciClickSuSquadra(e, {nome: key.squadra2, icona: key.icon2})}}>
                                                 <div className="squadra1-partite-icon" style={{backgroundImage:`url(${key.icon2})`}}></div>
                                                 <div className="squadra1-name">{key.squadra2}</div>
                                             </div>
