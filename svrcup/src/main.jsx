7bdfb366bf71763fedcec5674bc15c965de1f4bb
trinchierilucauni <trinchieri.2150132@studenti.uniroma1.it>
2026-06-18 12:50:43 +0200

    Correzione main e vercel.json

--- svrcup/src/main.jsx
+++ svrcup/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Navigate } from 'react-router-dom'
 import Partite from './Partite.jsx'
 import Classifica from "./Classifica.jsx";
 import InfoSquadra from "./InfoSquadra.jsx"
@@ -22,9 +23,6 @@ function AppLayout(){
     </>
 
   );
-}
-if (window.location.pathname !== "/") {
-  window.location.replace("/");
 }
 
 const router= createBrowserRouter([{
@@ -51,6 +49,10 @@ const router= createBrowserRouter([{
       path: "infopartita",
       element: <InfoPartita></InfoPartita>
     },
+    {
+      path: "*",
+      element: <Navigate to={"/"} replace/>
+    }
 
 
   ]
